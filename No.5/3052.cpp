2ee717a31a36139a85c26153abf88beaad55f18d
Jeonhyunjun <jhj091542@gmail.com>
2021-10-10 21:45:33 +0900

    코드 수정

--- No.5/3052.cpp
+++ No.5/3052.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
-int main(void){
-    int num, sum = 0, remainderArr[42] = {0,};
-    for(int i = 0; i < 10; i++){
-        std::cin>>num;
-        if(!remainderArr[num % 42]++)
-            sum++;
-    }
-    std::cout<<sum;
+using namespace std;
+
+int main()
+{
+	
+	int input , count = 0; // 입력 받는 수는 0으로 
+	int arr[42] = {0}; // 42를 나누는 수로 두니깐 42를 크기로하기 
+	
+	for(int i = 0 ; i < 10 ; i++) 
+	{
+		cin >> input; // input 변수를 입력 받기 
+		if(!arr [input % 42] ++) // 해당 값이 0이 아니라면 
+			count++; // 카운드 시작 ! 
+	}
+	cout << count; // 출력문  
+	
+	
+	return 0;
 }
