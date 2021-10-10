2b3dc16ecfc91433daaef00f889fdfc1d85712b7
Jeonhyunjun <jhj091542@gmail.com>
2021-10-10 21:16:00 +0900

    주석 추가  문제 이해가 왜이리 안되는거지 ..;;

--- No.5/2577.cpp
+++ No.5/2577.cpp
@@ -6,18 +6,18 @@ int main()
 	int a , b , c;
 	cin >> a >> b >> c;
 	
-	int arr[10] = {0};
+	int arr[10] = {0}; //0으로 초기화 
 
-	int	cl = a * b * c;
+    int	cl = a * b * c; // 주어진 a,b,c 추가 
 	
 	
-	while(cl != 0){
-		arr[cl % 10] += 1;
-		cl /= 10;
+	while(cl != 0){  // cl이 0이면 true 아니면 false  
+		arr[cl % 10] += 1; // 나머지 연산으로 갯수 더하기 
+		cl /= 10; // 결과를 10으로 나누기 
 	}
 	
-	for(int i = 0; i < 10;i ++){
-		cout << arr[i] << endl;
+	for(int i = 0; i < 10;i ++){ // arr사이즈에 맞게 10으로 추가하기 
+		cout << arr[i] << endl; // 한줄씩 출력하기 
 	}
     
 
