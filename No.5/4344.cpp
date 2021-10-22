f18553acb69cd0c650f9a30fe85be1569efa654a
Jeonhyunjun <jhj091542@gmail.com>
2021-10-22 23:13:28 +0900

    진짜 19번 줄에서 for 말고 fot이라고 쓴 내가 레전드다 진짜 ;

--- No.5/4344.cpp
+++ No.5/4344.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	int c, n, num;
-	int avg;
-	int score[1000] = { 0 };
-	double result;
-
-	cin >> c;
-	for (int i = 0; i < c; i++) {
-		avg = 0;
-		num = 0;
-		cin >> n;
-
-		for (int j = 0; j < n; j++) {
-			cin >> score[j];
-			avg = avg + score[j];
+int main()
+{
+	int test , stu , sco[1000] , avg = 0; //테스트 케이스 , 학생수 , 점수 , 평균 선언 
+	double ans; // 답
+	cin >> test;  
+	for(int i = 0; i < test; i++){ // 테스트 수 만큼 학생수 입력 
+		cin >> stu;
+		int sum = 0; // 합계 초기화 
+		
+		for(int j = 0; j < stu; j++){ //점수 입력받기 
+			cin >> sco[j]; // 점수 입력 받기 
+			sum += sco[j]; // 입력 받은 점수를 합계에 더함 
 		}
-		avg = avg / n;
-		for (int j = 0; j < n; j++) {
-			if (score[j] > avg)
-				num++;
+		avg = sum / stu; // 평균값은 합계 나누기 학생수 
+		double count = 0; 
+		for(int j = 0; j < stu; j++){
+			if(avg < sco[j]) // 점수가 평균보다 높을 시 count 는 더해지기 
+					count++;
 		}
-		result = (double)num / n * 100;
-
-		cout << fixed;
-		cout.precision(3);
-		cout << result << "%" << endl;
-	}
+		
+		ans = (count / stu) * 100; // 비율 계산 
+		cout.precision(3); // 소숫점 3자리 까지만 출력하 
+		cout << fixed;  
+		cout << ans << "%" << endl;
+	} 
+	
+	return 0;
 }
