17229ecadec913ff34f71ca822708aec683a2c89
Jeonhyunjun <jhj091542@gmail.com>
2021-10-24 00:42:46 +0900

    컴파일에러로 백준 다음페이지 만든 썰 풉니다 ㅎㅎ

--- No.6/15596.cpp
+++ No.6/15596.cpp
@@ -1,1 +1,12 @@
-
+#include <vector>
+	
+long long sum(std::vector<int> &a){ //a라는 이름의 vetor list 생성  
+		long long result = 0; // result 변수 초기화 
+	
+	
+		for(int i = 0; i < a.size(); i++){ // i가 a의 size보다 같거나 커질때까진 i는 증가 
+			result += a[i]; // 결과는 a의 index와 더합니당 
+		}
+	
+		return result;
+}
